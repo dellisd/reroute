84cea0b09f34c5763fe6abf0f9a0039727cc15b8
Derek Ellis <derekellis170@gmail.com>
2023-08-07 22:43:47 -0400

    Fix search bar select interaction

--- web/src/jsMain/kotlin/ca/derekellis/reroute/ui/AppStylesheet.kt
+++ web/src/jsMain/kotlin/ca/derekellis/reroute/ui/AppStylesheet.kt
@@ -6,6 +6,7 @@ import org.jetbrains.compose.web.css.fontFamily
 import org.jetbrains.compose.web.css.height
 import org.jetbrains.compose.web.css.left
 import org.jetbrains.compose.web.css.margin
+import org.jetbrains.compose.web.css.media
 import org.jetbrains.compose.web.css.mediaMaxWidth
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.position
@@ -32,9 +33,12 @@ object AppStylesheet : StyleSheet() {
     position(Position.Absolute)
     top(0.px)
     left(0.px)
-    width(100.percent)
-    height(100.percent)
-    property("pointer-events", "none")
+    width(512.px)
+    media(SmallMediaQuery) {
+      self style {
+        width(100.percent)
+      }
+    }
   }
 
   val SmallMediaQuery = mediaMaxWidth(600.px)
